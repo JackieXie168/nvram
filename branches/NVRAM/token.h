f6a1e197b394d0ec2dcf5666e1f6bc69211cd0fe
nobody <nobody@5d104e9d-bb48-0410-83af-96ddff762c08>
2007-12-27 19:37:59 +0000

    This commit was manufactured by cvs2svn to create branch 'NVRAM'.

---
+++ branches/NVRAM/token.h
@@ -0,0 +1,38 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include "list.h"
+
+
+/* Buffer sizes for reading a stream into tokens. */
+#define TOKEN_STREAM_BUFFER_SIZE_START     100
+#define TOKEN_STREAM_BUFFER_SIZE_INCREMENT 50
+
+/* Token types. */
+#define TOKEN_TYPE_NULL     0
+#define TOKEN_TYPE_EOF      1
+#define TOKEN_TYPE_EOL      2
+#define TOKEN_TYPE_STRING   3
+#define TOKEN_TYPE_KEYWORD  4
+#define TOKEN_TYPE_INTEGER  5
+
+typedef struct {
+	struct list_head list;
+	unsigned int     type;
+	unsigned int     line;
+	union {
+		wchar_t       *string;
+		unsigned int   keyword;
+		long           integer_number;
+	} data;	
+} token_t;
+
+/* Token operations. */
+token_t *token_new(void);
+void token_destroy(token_t *ptr);
+void token_destroy_list(token_t *ptr);
+void token_tokenize_stream(FILE *stream, struct list_head *token_list);
+int token_convert_keyword(token_t *token, wchar_t *keywords[]);
+int token_convert_integer(token_t *token);
+
+#endif
