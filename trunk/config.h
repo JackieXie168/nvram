1d95af68db1e174c902fb9fd3ba28a8cc714c239
jan <jan@5d104e9d-bb48-0410-83af-96ddff762c08>
2007-12-30 22:00:03 +0000

    Checksummen berechnen.

--- trunk/config.h
+++ trunk/config.h
@@ -3,12 +3,8 @@
 
 #include "detect.h"
 #include "list.h"
+#include "nvram.h"
 
-
-/* Config files. */
-#define CONFIG_BASE_FILENAME "./nvram.conf"
-#define CONFIG_PATH_LENGTH_MAX 1000
-#define CONFIG_NESTING_MAX 100
 
 /* Config operations. */
 void read_config(struct list_head *token_list, hardware_t *hardware_description, struct list_head *nvram_mapping);
